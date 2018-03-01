41b61e20d14aefcd2fd853419b1b70b18c508816
jerom <jerom@LAPTOP-4L91QNQD>
2018-03-01 15:17:16 +0100

    premier commit : résolution du point 2

---
+++ src/occases/DemoVue.java
@@ -0,0 +1,17 @@
+package occases;
+
+
+public class DemoVue {
+    public static void main(String[] args) {
+        GarageVue gv=new GarageVue();
+        gv.affMsg("salut");
+        Voiture v =new Voiture("111","AAA","AAA",111,111,111);
+        gv.affMsg(v);
+        gv.affMsg("nom :");
+        String nom=gv.getMsg();
+        gv.affMsg("le nom encode est "+nom);
+        String tel=gv.getMsg("tel :");
+        gv.affMsg("le telephone encode est "+tel);
+        
+      }
+}
